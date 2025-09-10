def3e5e7fdb638072b9bae04142249de7c458d42
Yash Gadhave <yash.gadhave0205@gmail.com>
2025-09-10 19:44:03 +0530

    Removed API key from code, added .env

--- ezchatbot.py
+++ ezchatbot.py
@@ -1,12 +1,17 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
 # ✅ Set your Gemini API Key here
-genai.configure(api_key="AIzaSyAdxfce6b6Pzoab64sBFBWtF2nnLyo6jPc")
+api_key = os.getenv("GENAI_API_KEY")
+genai.configure(api_key=api_key)
+
 
 # ✅ Load the Gemini model
 model = genai.GenerativeModel("gemini-1.5-flash")
